75d41d730f0ea233037c6eafe30abc36743d1c9f
ilvitorio <vdc_hernandez@hotmail.com>
2016-04-01 17:52:20 -0300

    Update Dictionary.r

--- Dictionary.r
+++ Dictionary.r
@@ -1,2 +1,2 @@
-dictionary <- c("ALUA","BHIP","BMA","CECO2","CGPA2","COME2","EDN","ERAR","GGAL","INDU","MIRG","PAMP","PETR","PESA","SAMI","TGS4","TRAN","TS","YPFD")
+dictionary <- c("ALUA","BHIP","BMA","CECO2","CGPA2","COME","EDN","ERAR","GGAL","INDU","MIRG","PAMP","PETR","PESA","SAMI","TGSU2","TRAN","TS","YPFD")
 names(dictionary) <- c("ALU","BHI","BMA","CEC","CGP","COM","EDN","ERA","GFG","IND","MIR","PAM","PBR","PSA","SAM","TGS","TRA","TS","YPF")
